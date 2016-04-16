fbb92eecc44891ed2c83e3a43de98a5878aa2200
maxamed daahir <Ducale@Own.(none)>
2016-04-16 16:30:13 +0300

    moved authentication code to auth module

--- public/app.module.js
+++ public/app.module.js
@@ -13,8 +13,6 @@ var myApp = angular.module('myApp', [
   'ngMaterial',
   'md.data.table',
   'angular-loading-bar',
-  'ui.router',
-  'satellizer'
 ]);
 
 
@@ -22,31 +20,7 @@ myApp.config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
   cfpLoadingBarProvider.includeSpinner = false;
 }])
 
-myApp.config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide) {
-
-  function redirectWhenLoggedOut($q, $injector) {
-    return {
-      responseError: function(rejection) {
-        var $state = $injector.get('$state');
-
-        var rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
-
-        angular.forEach(rejectionReasons, function(value, key) {
-          if (rejection.data.error === value) {
-            localStorage.removeItem('user');
-            $state.go('auth');
-          }
-        });
-        return $q.reject(rejection);
-      }
-    }
-  }
-
-  $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
-  $httpProvider.interceptors.push('redirectWhenLoggedOut');
-
-  $authProvider.loginUrl = 'api/authenticate';
-
+myApp.config(function($stateProvider, $urlRouterProvider){
   $urlRouterProvider.otherwise('/auth')
 
   $stateProvider
@@ -126,30 +100,9 @@ myApp.config(function($stateProvider, $urlRouterProvider, $authProvider, $httpPr
       controller: 'studentScoreController',
       controllerAs: 'vm'
     })
-
 })
 
 
-myApp.run(function($rootScope, $state) {
-
-  $rootScope.$on('$stateChangeStart', function(event, toState) {
-
-    var user = JSON.parse(localStorage.getItem('user'));
-
-    if (user) {
-      $rootScope.authenticated = true;
-      $rootScope.currentUser = user;
-
-      if (toState.name === "auth") {
-
-        event.preventDefault();
-        $state.go('home');
-      }
-    }
-
-  })
-
-})
 
 myApp.config(['$mdThemingProvider', function($mdThemingProvider) {
   'use strict';
--- public/auth/auth.module.js
+++ public/auth/auth.module.js
@@ -2,5 +2,53 @@
   'use strict';
 
   angular
-    .module('auth', []);
+    .module('auth', ['ui.router', 'satellizer'])
+
+  .config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide) {
+
+    function redirectWhenLoggedOut($q, $injector) {
+      return {
+        responseError: function(rejection) {
+          var $state = $injector.get('$state');
+
+          var rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
+
+          angular.forEach(rejectionReasons, function(value, key) {
+            if (rejection.data.error === value) {
+              localStorage.removeItem('user');
+              $state.go('auth');
+            }
+          });
+          return $q.reject(rejection);
+        }
+      }
+    }
+
+    $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
+    $httpProvider.interceptors.push('redirectWhenLoggedOut');
+
+    $authProvider.loginUrl = 'api/authenticate';
+  })
+
+  .run(function($rootScope, $state) {
+
+    $rootScope.$on('$stateChangeStart', function(event, toState) {
+
+      var user = JSON.parse(localStorage.getItem('user'));
+
+      if (user) {
+        $rootScope.authenticated = true;
+        $rootScope.currentUser = user;
+
+        if (toState.name === "auth") {
+
+          event.preventDefault();
+          $state.go('home');
+        }
+      }
+
+    })
+
+  })
+
 })();
