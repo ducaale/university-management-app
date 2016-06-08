ecfe16a6ec5aa005d17225455152f43bdb7375ee
maxamed daahir <Ducale@Own.(none)>
2016-06-08 16:31:43 +0300

    added separate sidenav elements for admin and students

--- public/other/directives/directives.js
+++ public/other/directives/directives.js
@@ -31,6 +31,61 @@
     return {
       restrict: 'E',
       templateUrl: 'other/partials/sidenav.html',
+      controllerAs: 'vm',
+      controller: function($mdSidenav, $rootScope, $state) {
+        var vm = this;
+        vm.adminStates = [{
+          name: 'students',
+          url: 'home.students'
+        }, {
+          name: 'guardians',
+          url: 'home.guardians'
+        }, {
+          name: 'staffs',
+          url: 'home.staffs'
+        }, {
+          name: 'fees',
+          url: 'home.fees'
+        }, {
+          name: 'scores',
+          url: 'home.scores'
+        }, {
+          name: 'attendance',
+          url: 'home.attendance'
+        }, {
+          name: 'other',
+          url: 'home.other'
+        }]
+
+        vm.studentStates = [{
+          name: 'scores',
+          url: 'home.studentScore'
+        }]
+
+        vm.getStates = function() {
+          var states = [];
+          if($rootScope.currentUser && $rootScope.currentUser.role == 'admin'){
+            states = vm.adminStates;
+          }
+          else {
+            states = vm.studentStates;
+          }
+          return states;
+        }
+
+        vm.getIcon = function() {
+          var icon = '';
+          if($rootScope.currentUser){
+            $rootScope.currentUser.gender_type == 'female' ? icon = 'avatars:svg-6' : icon = 'avatars:svg-1';
+          }
+          return icon;
+        }
+
+        vm.toggleSideNav = function() {
+            $mdSidenav('left').toggle();
+        }
+
+      }
     }
   };
 
