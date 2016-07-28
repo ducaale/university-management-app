b283f01c051d419b3049e138ab6ad9167e8d9204
maxamed daahir <Ducale@Own.(none)>
2016-07-28 19:04:35 +0300

    changed data format

--- app/Http/Controllers/studentMarkController.php
+++ app/Http/Controllers/studentMarkController.php
@@ -22,29 +22,88 @@ class studentMarkController extends Controller
 
       $semester = Input::get('semester');
       $examType = Input::get('examType');
+      $exams;
       $marks;
-
 
       if (isset($semester) ) {
         $marks = DB::table('marks')
-                    ->select('student_id', 'course_name', 'exam_type', 'semester', 'mark')
+                    ->select('course_name', 'exam_type', 'mark')
                     ->join('courses', 'courses.id', '=', 'marks.course_id')
                     ->join('exam_types', 'exam_types.id', '=', 'marks.exam_type_id')
                     ->where('student_id', '=', $id)
                     ->where('semester', '=', $semester)
+                    ->orderBy('course_name', 'asc')
                     ->get();
 
+        $exams = DB::table('marks')
+                    ->select('exam_type')
+                    ->join('exam_types', 'exam_types.id', '=', 'marks.exam_type_id')
+                    ->where('student_id', '=', $id)
+                    ->where('semester', '=', $semester)
+                    ->distinct()
+                    ->get();
       }
       else {
         $marks = DB::table('marks')
-                    ->select('student_id', 'course_name', 'exam_type', 'semester', 'mark')
+                    ->select('course_name', 'exam_type', 'semester', 'mark')
                     ->join('courses', 'courses.id', '=', 'marks.course_id')
                     ->join('exam_types', 'exam_types.id', '=', 'marks.exam_type_id')
                     ->where('student_id', '=', $id)
+                    ->orderBy('course_name', 'asc')
+                    ->get();
+
+        $exams = DB::table('marks')
+                    ->select('exam_type')
+                    ->join('exam_types', 'exam_types.id', '=', 'marks.exam_type_id')
+                    ->where('student_id', '=', $id)
+                    ->distinct()
                     ->get();
 
       }
 
-      return $marks;
+      /**
+       * process the $marks results into this format
+       * {course_name: course, grades: {exam_type: score,.....}
+       */
+      $mark_array = array();
+      $grade = array();
+      $next= $marks[1]->course_name;
+      $i = 0;
+      $size = sizeOf($marks);
+
+      foreach ($marks as $mark) {
+
+        $grade['course'] = $mark->course_name;
+        $grade['grade'][$mark->exam_type] = $mark->mark;
+        
+        if( $i != $size - 1 ) {
+          $next = $marks[$i + 1]->course_name;
+        }
+
+        if($mark->course_name != $next || $i == $size - 1) {
+          array_push($mark_array, $grade);
+          $grade = array();
+        } 
+        
+        $i++;
+      }
+
+      /**
+       * transform $exams into array
+       * exams: [ 'quiz', 'fullterm', .....]
+       */
+      $exam_array = array();
+      foreach($exams as $exam) {
+        array_push($exam_array, $exam->exam_type);
+      }
+        
+      /**
+       * combine examtypes and scores 
+       */ 
+      $result = array();
+      $result['exam_types'] = $exam_array;
+      $result['scores'] = $mark_array;
+
+      return $result;
     }
 }
